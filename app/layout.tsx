d409d50e27a3dd23a30f6a525a98659cf8c987fe
mvrga <marcusvrga@gmail.com>
2026-04-26 10:05:34 -0300

    chore(app): add Talent project verification meta tag
    Add the Talent project ownership verification meta tag to the homepage head.

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,10 @@ export default function RootLayout({
           name="apple-mobile-web-app-status-bar-style"
           content={appleWebAppStatusBarStyle}
         />
+        <meta
+          name="talentapp:project_verification"
+          content="1d779030e2b11df4a102d08b37f5f58b97efa0f499e1266d8096a18f6d055e86f72a2be3f6a9f416c4f2eb7c7f2c94956da3487bc43c5328519f7adef97bad2e"
+        />
       </head>
       <body>
         <GaitaPwaRegistration />
